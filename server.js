f8f4eb244fec18baf268c27d0d42a150475734cb
presannak <presannawenkatesank.21cse@kongu.edu>
2024-05-12 15:08:11 +0530

    last commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","*")
+    res.setHeader("Access-Control-Allow-Origin","https://earthmover.netlify.app/")
     next()
 })
 app.get('/',(req,res,next)=>{
