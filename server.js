aea0d939e1edc70a712e257cb6e1f7c63d7bb185
presannak <presannawenkatesank.21cse@kongu.edu>
2024-05-12 11:33:47 +0530

    added vercel.json

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const app = express();
 // const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors({
+  credentials:true,
+  origin:"*"
+}))
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:5173")
+    res.setHeader("Access-Control-Allow-Origin","*")
     next()
 })
 const pool = new Pool({
